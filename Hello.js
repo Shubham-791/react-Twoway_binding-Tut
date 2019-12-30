bf5ffec53ecad168efea1d06487d0abb87b00174
Shubham-791 <shubhamv791@gmail.com>
2019-12-30 16:40:16 +0530

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const hello = ({ name }) => <h1> {name}!</h1>;
+export default hello;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fydtvw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fydtvw)
