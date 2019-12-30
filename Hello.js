4099e947b60a19e4f8f44b9eb614d7cfea61fd33
Shubham-791 <shubhamv791@gmail.com>
2019-12-30 18:12:40 +0530

    Use State hook implemented with Input Component

--- Hello.js
+++ Hello.js
@@ -1,3 +1,4 @@
 import React from 'react';
 const hello = ({ name }) => <h1> {name}!</h1>;
+
 export default hello;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
@@ -22,5 +22,25 @@ class App extends Component {
     );
   }
 }
+const Input = () => {
+  const [inputText,setInputText] = useState("");
+  const [historyText,setHistoryText] = useState([]);
 
-render(<App />, document.getElementById('root'));
+  return (
+    <div>
+      <input type="text" onChange= {(e) => {
+        setInputText(e.target.value);
+        setHistoryText([...historyText,e.target.value]);
+      }}/><br/>
+      {inputText}
+      <hr/><br/><br/>
+      {
+        historyText.map( rec => {
+          return <div>{rec}</div>
+        })
+      }
+    </div>
+  ) 
+}
+
+render(<Input />, document.getElementById('root'));
